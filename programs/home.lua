1a60c8319e5e4362981e4e3cbdc6385f09b41d34
TheBiggerFish <clhnumber4@gmail.com>
2025-01-14 20:54:52 -0500

    Home program

--- programs/home.lua
+++ programs/home.lua
@@ -1,4 +1,4 @@
-local arguments = require("/api/arguments")
+local gps = require("gps")
 
 ---@returns nil
 function PrintUsage()
@@ -7,4 +7,54 @@ function PrintUsage()
     error()
 end
 
-local args = arguments.Interpret( arg )
+---@returns nil
+function PrintHome()
+    local x = settings.get("home.x")
+    local y = settings.get("home.y")
+    local z = settings.get("home.z")
+    if x == nil or y == nil or z == nil then
+        print("Home is not set")
+    else
+        print("Home is set to: " .. x .. " " .. y .. " " .. z)
+    end
+end
+
+---@returns nil
+function SetHomeHere()
+    x, y, z = gps.locate()
+    SetHome(x, y, z)
+end
+
+---@param x integer
+---@param y integer
+---@param z integer
+---@returns nil
+function SetHome(x, y, z)
+    settings.set("home.x", x)
+    settings.set("home.y", y)
+    settings.set("home.z", z)
+end
+
+if #arg < 2 then
+    PrintUsage()
+elseif arg[2] == "get" and #arg == 2 then
+    PrintHome()
+elseif arg[2] == "set" and #arg == 2 then
+    SetHomeHere()
+elseif arg[2] == "set" and #arg == 5 then
+    local x,y,z
+    x = tonumber(arg[3])
+    y = tonumber(arg[4])
+    z = tonumber(arg[5])
+    if x == nil or y == nil or z == nil then
+        PrintUsage()
+        error()
+    end
+    --- cast to integer
+    x = math.floor(x)
+    y = math.floor(y)
+    z = math.floor(z)
+    SetHome(x,y,z)
+else 
+    PrintUsage()
+end
