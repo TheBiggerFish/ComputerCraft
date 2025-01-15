dc9b3ed0fe01cf97e5861edcb88c61b1ce57c33d
TheBiggerFish <clhnumber4@gmail.com>
2025-01-14 20:37:45 -0500

    Set up CLI args

--- programs/home.lua
+++ programs/home.lua
@@ -1,3 +1,10 @@
 local arguments = require("/api/arguments")
 
+---@returns nil
+function PrintUsage()
+    local programName = arg[0] or fs.getName(shell.getRunningProgram())
+    print("Usages: \n" .. programName .. " get\n" .. programName .. " set\n" .. programName .. " set <x> <y> <z>")
+    error()
+end
+
 local args = arguments.Interpret( arg )
